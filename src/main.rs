a40f363b52b06259dbfa4a2abcfb366ab7d363ed
Alejandro Arias <alejandroarias@Alejandros-MacBook-Air.local>
2025-03-02 16:13:45 -0800

    Added main loop for cli

--- src/main.rs
+++ src/main.rs
@@ -3,6 +3,25 @@ mod models;
 use crate::db_utils::{connect_to_db, add_dummy_data};
 use rusqlite::{Result};
 
+fn run_cli() {
+    use std::io::{stdin, stdout, Write};
+    println!("$ Welcome to Workout tracker!");
+    println!("$ Type HELP for a list of commands");
+    let mut input = String::new();
+    let _=stdout().flush();
+    loop {
+        input.clear();
+        stdin().read_line(&mut input).expect("Error reading input");
+
+        if input.trim() == "HELP" {
+            println!("User entered: {}", input);
+            break;
+        }
+
+        println!("Invalid command, try again!");
+    }
+}
+
 fn main() -> Result<()> {
     println!("Hello, world! Welcome to Workout Tracker! Author: Alejandro Arias");
 
@@ -10,7 +29,8 @@ fn main() -> Result<()> {
     println!("Database connection successful!");
 
     println!("Initializing databse with dummy values...");
-    add_dummy_data(&_conn)?;
+   
+    run_cli();
 
     Ok(())
 }
